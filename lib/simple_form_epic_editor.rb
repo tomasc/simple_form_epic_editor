f0474508cedadcba375fb5e29bc6ae1cb26e4007
Asger Behncke Jacobsen <abj@asgers-mbp-2.fritz.box>
2014-07-14 15:54:29 +0200

    fix

--- lib/simple_form_epic_editor.rb
+++ lib/simple_form_epic_editor.rb
@@ -1,2 +1,12 @@
 require "simple_form_epic_editor/simple_form_epic_editor"
 require "simple_form_epic_editor/version"
+
+require "rails-assets-wysihtml5x"
+
+# ---------------------------------------------------------------------
+
+module SimpleForm
+  class FormBuilder
+    map_type :epic_editor, to: SimpleFormEpicEditor::EpicEditorInput
+  end
+end
